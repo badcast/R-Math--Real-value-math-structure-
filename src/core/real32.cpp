77027db6910eaa620cfb705a575be41cc4fb8921
badcast <badcast@github.com>
2021-02-01 04:20:00 +0600

    C

--- src/core/real32.cpp
+++ src/core/real32.cpp
@@ -1,6 +1,7 @@
 #include "../../include/rmath.hpp"
 
 using namespace rmath;
+using namespace rmath::func;
 
 real32::real32(){
 
@@ -12,44 +13,44 @@ real32::~real32(){
 
 real32::real32(const int& m, const int& p, const int& q)
 {
-    this->m = m;
-    this->p = p;
-    this->q = q;
+   this->m = m;
+   this->p = p;
+   this->q = q;
 }
 real32::real32(const int64_t& rhs)
 {
-    this->m = 0;
-    this->p = rhs;
-    this->q = 1;
+   this->m = 0;
+   this->p = rhs;
+   this->q = 1;
 }
 real32::real32(const uint64_t& rhs)
 {
-    this->m = 0;
-    this->p = rhs;
-    this->q = 1;
+   this->m = 0;
+   this->p = rhs;
+   this->q = 1;
 }
 real32::real32(const double& rhs)
 {
-    if (rhs == std::numeric_limits<double>::infinity()) {
-        *this = positiveInfinity;
-    } else if (rhs == -std::numeric_limits<double>::infinity()) {
-        *this = negativeInfinity;
-    } else {
-        m = 0;
-        p = int(rhs * 1000) - int(rhs * 100);
-        q = 900;
-        *this = to_prime(*this);
-    }
+   if (rhs == std::numeric_limits<double>::infinity()) {
+      *this = positiveInfinity;
+   } else if (rhs == -std::numeric_limits<double>::infinity()) {
+      *this = negativeInfinity;
+   } else {
+      m = 0;
+      p = int(rhs * 1000) - int(rhs * 100);
+      q = 900;
+      *this = to_prime(*this);
+   }
 }
 
 // operators
-real32& real32::operator+=(const real32& rhs) { return *this = *this + rhs; }
+real32& real32::operator+=(const real32& rhs) { return *this = func::add(*this, rhs); }
 
-real32& real32::operator-=(const real32& rhs) { return *this = *this - rhs; }
+real32& real32::operator-=(const real32& rhs) { return *this = func::sub(*this, rhs); }
 
-real32& real32::operator*=(const real32& rhs) { return *this = *this * rhs; }
+real32& real32::operator*=(const real32& rhs) { return *this = func::mul(*this,rhs); }
 
-real32& real32::operator/=(const real32& rhs) { return *this = *this / rhs; }
+real32& real32::operator/=(const real32& rhs) { return *this = func::div(*this, rhs); }
 
 real32::operator float() { return static_cast<float>(this->operator double()); }
 real32::operator double() { return func::to_double(*this); }
